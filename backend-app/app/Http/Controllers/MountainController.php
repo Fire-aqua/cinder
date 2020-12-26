cb5b0c102572e4b3462e98f9bdb9f959e28b0744
Fireaqua <fire-aqua@yandex.ru>
2020-12-26 21:06:47 +0400

    Forms&filters

--- backend-app/app/Http/Controllers/MountainController.php
+++ backend-app/app/Http/Controllers/MountainController.php
@@ -9,10 +9,23 @@ class MountainController extends Controller
 {
     public function index(Request $request)
     {
-        $mountains=Mountain::orderBy('name', 'desc')
+        $isIcy=$request->input('is_icy');
+        $minHeight = (int) $request->input('min_height');
+        $maxHeight = (int) $request->input('max_height');
+        
+        $mountains=Mountain::orderBy('name', 'asc')
+
+        ->when($isIcy=="true", function ($query) {
+            return $query->where('is_icy', true);
+        }) 
+        ->when($isIcy=="false", function ($query) {
+            return $query->where('is_icy', false);
+        })
+        
+        ->when($minHeight, fn($query)=> $query->where('height', '>=', $minHeight) )
+        ->when($maxHeight, fn($query)=> $query->where('height', '<=', $maxHeight) )
+        
         ->get();
-        return view('mountains', [
-            'mountainsArray' => $mountains
-            ]);
+        return view('mountains', compact('mountains', 'isIcy', 'minHeight', 'maxHeight'));
     }
 }
--- backend-app/resources/views/mountains.blade.php
+++ backend-app/resources/views/mountains.blade.php
@@ -1,6 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
+<div>
+    <form action="{{url('mountains')}}" method="GET" class="form">
+        <div class="row mb-3">
+            <div class="col">
+                <input placeholder="Минимальная высота" class="form-control" type="number" name="min_height" value="{{$minHeight!=0 ? $minHeight : ''}}">
+            </div>
+            <div class="col">
+                <input placeholder="Максимальная высота" class="form-control" type="number" name="max_height" value="{{$maxHeight!=0 ? $maxHeight : ''}}">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-check form-check-inline">
+                    <input name="is_icy" type="radio" value="true" {{$isIcy=="true" ? 'checked': ''}}>
+                    <label class="form-check-label ml-2">Лед</label>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-check form-check-inline">
+                    <input name="is_icy" type="radio" value="false" {{$isIcy=="false" ? 'checked' : ''}}>
+                    <label class="form-check-label ml-2">Без льда</label>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-check form-check-inline" {{$isIcy!="true"&&$isIcy!="false" ? 'checked' : ''}}>
+                    <input name="is_icy" type="radio">
+                    <label class="form-check-label ml-2">Неважно</label>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <button type="submit" class="btn btn-outline-primary btn-block">Искать</button>
+            </div>
+        </div>
+    </form>
+    
+</div>
 <div class="table-responsive ">
     <table class="table table-striped table-bordered">
         <tr>
@@ -8,7 +46,7 @@
             <th>Высота (метры)</th>
             <th>Наличие льда</th>
         </tr>
-        @foreach ($mountainsArray as $mountain)
+        @foreach ($mountains as $mountain)
             <tr>
                 <td>{{$mountain->name}}</td>
                 <td>{{$mountain->height}}</td>
